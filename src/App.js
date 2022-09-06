2525ddf6116f5df43f153cc316bfd7e624272f4c
Paul Nizet <polonizet@gmail.com>
2022-09-06 15:37:19 +0200

    create Table component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import './App.css';
-import './index.css'
-import React, { useState } from 'react'
-import contact from './contacts.json';
-
-
+import "./App.css";
+import "./index.css";
+import React, { useState } from "react";
+import contact from "./contacts.json";
+import Table from "./compoments/Table";
 
 function App() {
   const [contacts, setContacts] = useState(contact.slice(0, 5));
 
   return (
     <div className="App">
-    <p>IronContacts</p>
-      <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Rating</th>
-        </tr>
-          {contacts.map((contact) => {
-          return (
-            <tr>
-              <td className="image" colspan="1">
-                <img src={contact.pictureUrl} alt="contact" />
-              </td>
-              <td colspan="2">
-                <h3>{contact.name}</h3>
-              </td>
-              <td colspan="3">
-                  <p>{Math.floor(contact.popularity)}</p>
-              </td>
-            </tr>
-          );
-        })}
-      </table>
+      <p>IronContacts</p>
+      <Table contacts={contacts} />
     </div>
   );
 }
---
+++ src/compoments/Table.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Table = ({ contacts }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td>Picture</td>
+                    <td>Name</td>
+                    <td>Rating</td>
+                    <td>Won Oscar</td>
+                    <td>Won Emmy</td>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => {
+                    return (
+                        <tr>
+                            <td>
+                                <img src={contact.pictureUrl} alt="profilePic" />
+                            </td>
+                            <td>{contact.name}</td>
+                            <td>{contact.popularity.toFixed(2)}</td>
+                            <td>{contact.wonOscar ? "🏆" : " "}</td>
+                            <td>{contact.wonEmmy ? "🏆" : " "}</td>
+                            {/* <td>
+                  <button onClick={() => props.onDeleteRow(contact.id)}>
+                    Delete
+                  </button>
+                </td> */}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
